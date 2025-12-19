aaec64c3f5cae56c62f6b69b1114fcc4dd8f96a2
Nitin Baluni <nitin.baluni@mail.vinove.com>
2025-12-19 14:46:12 +0530

    added new file for cs listing

---
+++ master/cs-listings.php
@@ -0,0 +1,41 @@
+<?php require_once 'common/inc/config.inc.php'; ?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Hire PHP Developers | Dedicated PHP developers / Programmers India - @ValueCoders</title>
+    <meta name="description"
+      content="Want to hire PHP developers for your web development project? Hire PHP programmers / developers from us & build scalable & secure web apps. 17+ Yrs | 4200+ projects | Strict NDAs | dedicated PHP Programmers." />
+    <meta name="keywords"
+      content="Hire PHP developers, Hire PHP programmers, Hire PHP developers India, Hire PHP programmers India, Offshore PHP developers, Offshore PHP programmers, Offshore PHP developers India, Offshore PHP programmers India, Dedicated PHP developers, dedicated PHP Programmers" />
+    <meta property="og:title"
+      content="Hire PHP Developers | Dedicated PHP developers / Programmers India - @ValueCoders" />
+    <?php require_once './include/header-files.php'; ?>
+    <link rel="preload stylesheet" type="text/css" href="./css/index-v8.css" defer />
+    <link rel="preload stylesheet" type="text/css" href="assets/cs-landing.css" defer />
+    <link rel="preload stylesheet" type="text/css" href=" https://cdnjs.cloudflare.com/ajax/libs/Glide.js/3.0.1/css/glide.core.css" defer />
+    <script>
+    var vcObj = {
+    "_version": "1763614476",
+    "tpl_url": "http://localhost/markup-dom/valuecoders/master",
+    "web_url": "http://localhost/markup-dom/valuecoders/master",
+    "admin_ajax": "https://www.valuecoders.com/staging/wp-admin/admin-ajax.php",
+    "page_tpl": "tpl-homev6.0.php",
+    "is_mobile": "false",
+    "_env": "staging",
+    "_v3_blog_post": "",
+    "_post_id": "19655",
+    "_cs_data": null,
+    "_blog_tag": ""
+    };
+    </script>
+  
+  <script defer src="menu.js"></script>
+  </head>
+  <body>
+  <?php require_once './include/menu-v8.php'; ?>    
+  <!--  -->
+  <?php require_once 'include/footer.php'; ?> 
+  </body>
+</html>
